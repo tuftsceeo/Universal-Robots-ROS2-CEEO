eae955896e978ec0cf757b1f2807807d26eb4e34
theLiamFC <147665958+theLiamFC@users.noreply.github.com>
2024-07-12 16:40:15 -0400

    multiple point trajectories, float support for duration parameter

--- MyUR3e.py
+++ MyUR3e.py
@@ -9,10 +9,7 @@ from ik_solver.ur_kinematics import URKinematics
 from builtin_interfaces.msg import Duration
 from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
 from control_msgs.action import FollowJointTrajectory
-
-
-class MyException(Exception):
-    pass
+from sensor_msgs.msg import JointState
 
 
 class MyUR3e(rclpy.node.Node):
@@ -48,6 +45,7 @@ class MyUR3e(rclpy.node.Node):
         self._get_result_future = None
         self.done = True
         self.id = 0
+        self.joint_states = JointStates()
 
     @staticmethod
     def pointdeg2rad(point):
@@ -57,37 +55,101 @@ class MyUR3e(rclpy.node.Node):
             point2.append(math.radians(point[i]))
         return point2
 
-    def move_global(self, cords):
-        if len(cords) == 6:
+    # might need to add support for list of velocities if it seems useful
+    def move_global(
+        self,
+        cords,
+        units="radians",
+        velocities=[0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+        time=5,
+    ):
+        # get current pose form /joint_states topic
+        current_pose = self.joint_states.get()["position"]
+        self.get_logger().debug(f"Current Pose: {current_pose}")
+
+        # check cords for single or multiple points
+        if type(cords[0]) == int:
+            joint_positions = self.solve_ik(cords, current_pose)
+        elif type(cords[0]) == list:
+            joint_positions = []
+            for i,cord in enumerate(cords): # compute ik for each cord
+                try: # ideally just don't add any point for invalid IK
+                    if i == 0: # for first cord, use current pose as q_guess
+                        joint_positions.append(self.solve_ik(cord, current_pose))
+                    else: # for following cords use last pose as q_guess
+                        joint_positions.append(
+                            self.solve_ik(cord, joint_positions[-1])
+                        )
+                except Exception as e:
+                    self.get_logger().debug(e)
+        else:
+            raise ValueError("Invalid coordinate format")
+
+        # send off target joint angles
+        self.move_joints(
+            joint_positions, units=units, velocities=velocities, time=time
+        )
+
+    # Potential BUG with invalid IK cords - needs testing
+    # TODO add support for more rotation formats
+    # ??? what is the most intuitive way of orienting the end effector?
+    def solve_ik(self, cords, q_guess):
+        if len(cords) == 6:  # convert 3 rotations around cartesian axes to quaternion
             r = R.from_euler("xyz", cords[3:6], degrees=True)
             quat = r.as_quat(scalar_first=True).tolist()
             cords = cords[0:3] + quat
+
+        # use class ik solver to find joint positions from x,y,z,quaternion
         joint_positions = self.ik_solver.inverse(
-            cords, False
-        )  # missing q_guess=joint_angles_rad could be essential
-        if joint_positions.any():
-            self.move_joints(joint_positions.tolist())
-        else:
-            raise MyException("IK solution not found")
+            cords, False, q_guess=q_guess
+        )
 
-    def move_joints(self, joint_positions):
-        self.get_logger().debug(f"Moving to joint angles {joint_positions}")
-        goal = self.make_goal(joint_positions)
-        self.execute_goal(goal)
-        self.wait(self)
+        if joint_positions is None:
+            raise RuntimeError("IK solution not found")
 
-    # @staticmethod # not used because goal.joint_names relies on instance's self.joints
-    def make_goal(
+        return joint_positions.tolist()
+
+    def move_joints(
         self,
         joint_positions,
         units="radians",
         velocities=[0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
-        time_from_start=5,
+        time=5,
     ):
-        goal = JointTrajectory()
-        goal.joint_names = self.joints
+        self.get_logger().debug(f"Moving to joint angles {joint_positions}")
+
+        # make trajectory and append points
+        trajectory = JointTrajectory()
+        trajectory.joint_names = self.joints
+
+        # make trajectories for single and multiple point instances
+        if type(joint_positions[0]) == int:
+            trajectory = self.make_point(
+                joint_positions,
+                trajectory,
+                units=units,
+                velocities=velocities,
+                time=time,
+            )
+        elif type(joint_positions[0]) == list:
+            for pos in joint_positions:
+                trajectory = self.make_point(
+                    pos, trajectory, units=units, velocities=velocities, time=time
+                )
+
+        self.execute_trajectory(trajectory)
+        self.wait(self)
+
+    # append point to inputted trajectory object
+    def make_point(
+        self,
+        joint_positions,
+        trajectory,
+        units="radians",
+        velocities=[0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+        time=5,
+    ):
         point = JointTrajectoryPoint()
-        self.id += 1
 
         if units == "radians":
             point.positions = joint_positions
@@ -95,19 +157,25 @@ class MyUR3e(rclpy.node.Node):
             point.positions = self.pointdeg2rad(joint_positions)
 
         point.velocities = velocities
-        point.time_from_start = Duration(sec=time_from_start, nanosec=0)
-        goal.points.append(point)
-        return goal
+        sec = int(time - (time % 1))
+        nanosec = int(time % 1 * 1000000000)
+        point.time_from_start = Duration(sec=sec, nanosec=nanosec)
 
-    def execute_goal(self, goal):
-        self.get_logger().info(f"Goal #{self.id}: Executing")
-        self.done = False  # this was .static ??? could be error
+        trajectory.points.append(point)
 
-        this_goal = FollowJointTrajectory.Goal()
-        this_goal.trajectory = goal
+        return trajectory
+
+    def execute_trajectory(self, trajectory):
+        self.id += 1
+
+        self.get_logger().info(f"Trajectory #{self.id}: Executing")
+        self.done = False 
+
+        goal = FollowJointTrajectory.Goal()
+        goal.trajectory = trajectory
 
         self._action_client.wait_for_server()  # this was missing
-        self._send_goal_future = self._action_client.send_goal_async(this_goal)
+        self._send_goal_future = self._action_client.send_goal_async(goal)
         self.get_logger().debug(f"Sent goal #{self.id}")
         self._send_goal_future.add_done_callback(self.goal_response_callback)
 
@@ -132,7 +200,6 @@ class MyUR3e(rclpy.node.Node):
     def wait(self, client):
         rclpy.spin_once(client)
         while not client.done:
-            time.sleep(0.1)
             rclpy.spin_once(client)
 
     @staticmethod
@@ -149,3 +216,38 @@ class MyUR3e(rclpy.node.Node):
             return "PATH_TOLERANCE_VIOLATED"
         if error_code == FollowJointTrajectory.Result.GOAL_TOLERANCE_VIOLATED:
             return "GOAL_TOLERANCE_VIOLATED"
+
+
+class JointStates(rclpy.node.Node):
+    """
+    Subscribe to the joint_states topic
+    """
+
+    def __init__(self):
+        super().__init__("SubscriberNode")
+        self.subscription = self.create_subscription(
+            JointState, "joint_states", self.listener_callback, 10
+        )
+        self.states = None
+        self.done = False
+
+    def listener_callback(self, msg):
+        data = {}
+        data["name"] = [msg.name[5]] + msg.name[0:5]
+        data["position"] = [msg.position[5]] + msg.position[0:5].tolist()
+        data["velocity"] = [msg.velocity[5]] + msg.velocity[0:5].tolist()
+        data["effort"] = [msg.effort[5]] + msg.effort[0:5].tolist()
+
+        self.states = data
+        self.done = True
+
+    def get(self):
+        self.wait(self)
+        self.done = False
+        return self.states
+
+    def wait(self, client):
+        rclpy.spin_once(client)
+        while not client.done:
+            rclpy.spin_once(client)
+            self.get_logger().debug(f"Waiting for joint_states_client")
